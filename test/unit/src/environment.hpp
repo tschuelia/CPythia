672d22f2091c61fef0737e6d6330f4c491cdc8f0
Julia <schmid.julia96@gmail.com>
2024-12-03 16:26:16 +0100

    update tests for new predictor

--- test/unit/src/environment.hpp
+++ test/unit/src/environment.hpp
@@ -22,9 +22,9 @@ std::string example_msa() {
 
 std::list<PredAttr> dna_msas() {
     std::list<PredAttr> dna_msas = {
-            std::make_tuple(std::string(STRINGIFY(DATAPATH/DNA_1.phy)), 0.69, 1.0, 0.77),
-            std::make_tuple(std::string(STRINGIFY(DATAPATH/DNA_2.phy)), 0.29, 0.88, 0.09),
-            std::make_tuple(std::string(STRINGIFY(DATAPATH/DNA_3.phy)), 0.0, 0.01, 0.01)
+            std::make_tuple(std::string(STRINGIFY(DATAPATH/DNA_1.phy)), 0.70, 1.0, 0.73),
+            std::make_tuple(std::string(STRINGIFY(DATAPATH/DNA_2.phy)), 0.33, 1.0, 0.03),
+            std::make_tuple(std::string(STRINGIFY(DATAPATH/DNA_3.phy)), 0.0, 0.04, 0.02)
     };
 
     return dna_msas;
@@ -32,9 +32,9 @@ std::list<PredAttr> dna_msas() {
 
 std::list<PredAttr> aa_msas() {
     std::list<PredAttr> aa_msas = {
-            std::make_tuple(std::string(STRINGIFY(DATAPATH/AA_1.phy)), 0.27, 0.75, 0.02),
-            std::make_tuple(std::string(STRINGIFY(DATAPATH/AA_2.phy)), 0.27, 0.73, 0.01),
-            std::make_tuple(std::string(STRINGIFY(DATAPATH/AA_3.phy)), 0.38, 1.0, 0.07),
+            std::make_tuple(std::string(STRINGIFY(DATAPATH/AA_1.phy)), 0.30, 0.88, 0.02),
+            std::make_tuple(std::string(STRINGIFY(DATAPATH/AA_2.phy)), 0.28, 0.96, 0.02),
+            std::make_tuple(std::string(STRINGIFY(DATAPATH/AA_3.phy)), 0.40, 1.0, 0.07),
     };
 
     return aa_msas;
